0e47f6ae7b20087e9ea70071ebd0399ad78c77dd
Yadusha21 <1221205086@soffice.mmu.edu.my>
2026-01-12 00:29:46 +0800

    cust

--- customer.cpp
+++ customer.cpp
@@ -8,4 +8,21 @@ struct orderItems
     int id;
     string name;
     double price;
+};
+
+struct Node
+{
+    int orderID;
+    string food;
+    Node* next;
 }
+
+class User
+{
+    protected:
+       string username;
+    public:
+      User() { username = "Guest";}
+      virtual ~User() {}
+      virtual void menu() = 0;
+};
