03bd4b11901ab70aec90880b63efd99e9e60d419
Yadusha21 <1221205086@soffice.mmu.edu.my>
2026-01-12 17:22:02 +0800

    customer selection

--- customer.cpp
+++ customer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -12,8 +13,7 @@ struct orderItems
 
 struct Node
 {
-    int orderID;
-    string food;
+    Order data;
     Node* next;
 }
 
@@ -21,8 +21,95 @@ class User
 {
     protected:
        string username;
+       string password;
+
     public:
-      User() { username = "Guest";}
-      virtual ~User() {}
-      virtual void menu() = 0;
+      User() { username = " "; password = " ";}
+      virtual void login() = 0;
+      virtual ~User() {};
 };
+
+class Customer : public User {
+    private: 
+       Node* head;
+    public: 
+       Customer() : head(NULL) {}
+
+    void search(int id)
+    {
+        Node* current = head;
+        while* (current != NULL) {
+            if (current-> data.ID == id)
+            {
+            cout << "Order Found: " << current-> data.name << endl;
+            return;
+            }
+            current = current -> next;
+        }
+        cout << "Order ID not found." << endl;      
+
+    }
+};
+
+int main()
+{
+    int choice;
+    Customer customerSel;
+
+    while (true)
+    {
+        cout << "============================" << endl;
+        cout << "     WELCOME TO KARABU      " << endl;
+        cout << "============================" << endl;
+        cout << "1. LOGIN    " << endl;
+        cout << "2. REGISTER " << endl;
+        cout << "3. ADMIN    " << endl;
+        cout << "4. STAFF    " << endl;
+        cout << "1. EXIT     " << endl;
+        cout << "Please enter your selection: ";
+
+        try{
+           
+            if( cin >> choice)
+            {
+                throw "Invalid selection! Please re-enter your selection:";
+            }
+
+            if(choice == 1)
+            {
+                customerSel.login();
+            }
+            else if(choice == 2)
+            {
+                customerSel.register();
+            }
+            else if(choice == 3)
+            {
+                cout << amdin.login();
+            }
+            else if(choice == 4)
+            {
+                cout << staff.login();
+            }
+            else if(choice == 5)
+            {
+                cout << "Exiting system. Thank you for using KARABU!" << endl;
+            }
+            else
+            {
+                cout << "Selectipn not available. Please re-enter your selection: " << endl;
+
+            }
+        }
+        catch(const char* error)
+        {
+            cout << "Error: " << error << endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
